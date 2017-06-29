e3d9497a645ec63b027c49c0c2c9c85a77e87672
Peter Amstutz <peter.amstutz@curoverse.com>
2017-06-28 22:12:32 -0400

    Support aliasing, assigning output vars.

--- cwlnote.py
+++ cwlnote.py
@@ -18,141 +18,189 @@ class Redirect(object):
     def __repr__(self):
         return "Redirect%s%s" % (self.pipe, self.fn)
 
-def main(argv):
-    f = open(argv[1])
-    cont = ""
-    cmds = []
-    for l in f:
-        l = l.strip()
-        if not l:
-            continue
-        cont += l
-        if l.endswith("\\"):
-            cont += " "
-            continue
-        sp = cont.split(" ")
-        pieces = []
-        while sp:
-            n = sp.pop(0)
-            if not n:
-                continue
-            if n.startswith("$("):
-                v = Var()
-                n = n[2:]
-                while not n.endswith(")"):
-                    v.pieces.append(n)
-                    n = sp.pop(0)
-                v.pieces.append(n[:-1])
-                pieces.append(v)
-            elif n[0] in ("<", ">"):
-                if len(n) == 1:
-                    n += sp.pop(0)
-                pieces.append(Redirect(n[0], n[1:]))
-            elif n[0] == "|":
-                if len(n) == 1:
-                    n += sp.pop(0)
-                pieces.append(n)
-            else:
-                pieces.append(n)
-        cmds.append(pieces)
+class Translate(object):
+    def load(self, fn):
+        f = open(fn)
         cont = ""
+        self.cmds = []
+        for l in f:
+            l = l.strip()
+            if not l:
+                cont = ""
+                continue
 
-    inputs = {}
-    outputs = {}
-    config = {}
-    binds = {}
-    steps = []
+            if l[0] == "#":
+                self.cmds.append([l])
+                cont = ""
+                continue
 
-    wf = {}
+            cont += l
+            if l.endswith("\\"):
+                cont += " "
+                continue
+            sp = cont.split(" ")
 
-    for c in cmds:
-        if c[0] in ("DockerPull",):
-            config[c[0]] = c[1]
-        elif c[0] in ("Output",):
-            outputs[c[1]] = {
-                "type": binds[c[1]].pieces[1],
-                "outputSource": binds[c[1]].pieces[0]
-            }
-        else:
-            tool = {
-                "id": "tool",
-                "class": "CommandLineTool",
-                "inputs": {
-                },
-                "requirements": {},
-                "hints": {},
-                "outputs": {
-                    "out": {
-                        "type": "Directory",
-                        "outputBinding": {
-                            "glob": "."
-                        }
-                    }
-                },
-                "arguments": []
-            }
+            if "=" in sp[0]:
+                cap = cont.split("=", 2)
+                self.cmds.append(["=", cap[0], cap[1]])
+                cont = ""
+                continue
 
-            if "DockerPull" in config:
-                tool["hints"]["DockerRequirement"] = {"dockerPull": config["DockerPull"]}
-
-            toolin = {}
-            for elm in c:
-                if isinstance(elm, Var):
-                    if '/' in elm.pieces[0]:
-                        srcstep, path = elm.pieces[0].split('/', 1)
-                        tool["arguments"].append("$(inputs.%s.path)/%s" % (srcstep, path))
-                        tool["inputs"][srcstep] = "Directory"
-                        toolin[srcstep] = binds[srcstep].pieces[0]
-                    else:
-                        inpvar = elm.pieces[0]
-                        if inpvar in binds:
-                            var = binds[inpvar]
-                        else:
-                            var = elm
-                            binds[inpvar] = var
-                            inputs[inpvar] = var.pieces[1]
-
-                        tool["inputs"][inpvar] = var.pieces[1]
-
-                        if len(elm.pieces) > 2 and elm.pieces[1] in ("boolean", "boolean?"):
-                            tool["arguments"].append({
-                                "prefix": elm.pieces[2],
-                                "valueFrom": "$(inputs.%s)" % inpvar
-                            })
-                        else:
-                            tool["arguments"].append("$(inputs.%s)" % inpvar)
-
-                        toolin[inpvar] = var.pieces[0]
-                elif isinstance(elm, Redirect):
-                    if elm.pipe == ">":
-                        tool["stdout"] = elm.fn
-                elif elm[0] == "|":
-                    tool["arguments"].append({"shellQuote": False, "valueFrom": "|"})
-                    tool["arguments"].append(elm[1:])
-                    tool["hints"]["ShellCommandRequirement"] = {}
+            pieces = []
+            while sp:
+                n = sp.pop(0)
+                if not n:
+                    continue
+                if n.startswith("${"):
+                    v = Var()
+                    n = n[2:]
+                    while not n.endswith("}"):
+                        v.pieces.append(n)
+                        n = sp.pop(0)
+                    v.pieces.append(n[:-1])
+                    pieces.append(v)
+                elif n[0] in ("<", ">"):
+                    if len(n) == 1:
+                        n += sp.pop(0)
+                    pieces.append(Redirect(n[0], n[1:]))
+                elif n[0] == "|":
+                    if len(n) == 1:
+                        n += sp.pop(0)
+                    pieces.append(n)
                 else:
-                    tool["arguments"].append(elm)
+                    pieces.append(n)
+            self.cmds.append(pieces)
+            cont = ""
 
-            stepid = "step_"+c[0]
-            step = {
-                "id": stepid,
-                "run": tool,
-                "in": toolin,
-                "out": ["out"]
-            }
-            steps.append(step)
-            laststep = stepid
-            binds[c[0]] = Var(["%s/%s" % (stepid, "out"), "Directory"])
+    def start_tool(self, stepid):
+        self.toolin = {}
+        self.toolout = ["out"]
+        self.stepid = stepid
+        self.tool = {
+            "id": "tool",
+            "class": "CommandLineTool",
+            "inputs": {
+            },
+            "requirements": {},
+            "hints": {},
+            "doc": "",
+            "outputs": {
+                "out": {
+                    "type": "Directory",
+                    "outputBinding": {
+                        "glob": "."
+                    }
+                }
+            },
+            "arguments": []
+        }
+        if "DockerPull" in self.config:
+            self.tool["hints"]["DockerRequirement"] = {"dockerPull": self.config["DockerPull"]}
 
-    wf = {
-        "cwlVersion": "v1.0",
-        "class": "Workflow",
-        "steps": steps,
-        "inputs": inputs,
-        "outputs": outputs
-    }
 
-    print json.dumps(wf, indent=4)
+    def emit(self):
+        self.config = {}
+        self.binds = {}
+
+        inputs = {}
+        outputs = {}
+        steps = []
+
+        wf = {}
+
+        self.tool = None
+
+        comment_block = False
+
+        for c in self.cmds:
+            if c[0] in ("DockerPull",):
+                self.config[c[0]] = c[1]
+            elif c[0] in ("Output",):
+                outputs[c[1]] = {
+                    "type": self.binds[c[1]].pieces[1],
+                    "outputSource": self.binds[c[1]].pieces[0]
+                }
+            elif c[0] == "=":
+                self.tool["outputs"][c[1]] = {
+                    "type": "File",
+                    "outputBinding": {
+                        "glob": c[2]
+                    }
+                }
+                self.toolout.append(c[1])
+                self.binds[c[1]] = Var(["%s_step/%s" % (self.stepid, c[1]), "File"])
+            elif c[0][0] == "#":
+                if not comment_block:
+                    self.start_tool(c[0][1:].strip())
+                    comment_block = True
+                else:
+                    self.tool["doc"] += c[0][1:] + "\n"
+            else:
+                if not comment_block:
+                    self.start_tool(c[0])
+
+                for elm in c:
+                    if isinstance(elm, Var):
+                        if '/' in elm.pieces[0]:
+                            srcstep, path = elm.pieces[0].split('/', 1)
+                            self.tool["arguments"].append("$(inputs.%s.path)/%s" % (srcstep, path))
+                            self.tool["inputs"][srcstep] = "Directory"
+                            self.toolin[srcstep] = self.binds[srcstep].pieces[0]
+                        else:
+                            inpvar = elm.pieces[0]
+                            if inpvar in self.binds:
+                                var = self.binds[inpvar]
+                            else:
+                                var = elm
+                                self.binds[inpvar] = var
+                                inputs[inpvar] = var.pieces[1]
+
+                            self.tool["inputs"][inpvar] = var.pieces[1]
+
+                            if len(elm.pieces) > 2 and elm.pieces[1] in ("boolean", "boolean?"):
+                                self.tool["arguments"].append({
+                                    "prefix": elm.pieces[2],
+                                    "valueFrom": "$(inputs.%s)" % inpvar
+                                })
+                            else:
+                                self.tool["arguments"].append("$(inputs.%s)" % inpvar)
+
+                            self.toolin[inpvar] = var.pieces[0]
+                    elif isinstance(elm, Redirect):
+                        if elm.pipe == ">":
+                            self.tool["stdout"] = elm.fn
+                    elif elm[0] == "|":
+                        self.tool["arguments"].append({"shellQuote": False, "valueFrom": "|"})
+                        self.tool["arguments"].append(elm[1:])
+                        self.tool["hints"]["ShellCommandRequirement"] = {}
+                    else:
+                        self.tool["arguments"].append(elm)
+
+                step = {
+                    "id": self.stepid+"_step",
+                    "run": self.tool,
+                    "in": self.toolin,
+                    "out": self.toolout
+                }
+                steps.append(step)
+                self.binds[self.stepid] = Var(["%s/%s" % (self.stepid+"_step", "out"), "Directory"])
+                comment_block = False
+
+        wf = {
+            "cwlVersion": "v1.0",
+            "class": "Workflow",
+            "steps": steps,
+            "inputs": inputs,
+            "outputs": outputs
+        }
+
+        return wf
+
+
+def main(argv):
+    t = Translate()
+    t.load(sys.argv[1])
+    print json.dumps(t.emit(), indent=4)
 
     return 0
 
